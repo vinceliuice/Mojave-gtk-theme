9bf028666785160ff3f8efc1d0e54422053656cd
Nikita Zlobin <nick87720z@gmail.com>
2022-12-24 22:30:54 +0500

    fix: Typo in thumbnails render jobs code

--- src/assets/cinnamon/thumbnails/render-thumbnails.sh
+++ src/assets/cinnamon/thumbnails/render-thumbnails.sh
@@ -11,7 +11,7 @@ for theme in '' '-blue' '-purple' '-pink' '-red' '-orange' '-yellow' '-green' '-
   for light in 'Light' 'Dark'; do
     obj="thumbnail-${light}${theme}"
 
-    [ $(jobs -p | wc -l) -ge ${BUILD_THREADS} ]; then wait; fi
+    if [ $(jobs -p | wc -l) -ge ${BUILD_THREADS} ]; then wait; fi
     echo Rendering "$obj.png"
     $INKSCAPE --export-id=${obj@L} --export-id-only --export-filename=$obj.png $SRC_FILE >/dev/null 2>&1 &&
     $OPTIPNG -o7 --quiet $obj.png &
