e2fa4f2b05078ae76b3bc30dabada02d051d3ee6
Nikita Zlobin <nick87720z@gmail.com>
2022-12-25 22:18:58 +0500

    render-unity: Fix symlink creation

--- src/assets/unity/render-assets.sh
+++ src/assets/unity/render-assets.sh
@@ -30,30 +30,18 @@ done
 
 # links
 cd $ASSETS_DIR
-ln -s close.svg close_focused_normal.svg
-ln -s close_focused_prelight.svg close_unfocused_prelight.svg
-ln -s close_focused_pressed.svg close_unfocused_pressed.svg
-ln -s maximize.svg maximize_focused_normal.svg
-ln -s maximize_focused_prelight.svg maximize_unfocused_prelight.svg
-ln -s maximize_focused_pressed.svg maximize_unfocused_pressed.svg
-ln -s minimize.svg minimize_focused_normal.svg
-ln -s minimize_focused_prelight.svg minimize_unfocused_prelight.svg
-ln -s minimize_focused_pressed.svg minimize_unfocused_pressed.svg
-ln -s unmaximize.svg unmaximize_focused_normal.svg
-ln -s unmaximize_focused_prelight.svg unmaximize_unfocused_prelight.svg
-ln -s unmaximize_focused_pressed.svg unmaximize_unfocused_pressed.svg
-#ln -s shade.svg shade_focused.svg
-#ln -s shade.svg shade_focused_normal.svg
-#ln -s shade_focused_prelight.svg shade_unfocused_prelight.svg
-#ln -s shade_focused_pressed.svg shade_unfocused_pressed.svg
-#ln -s unshade.svg unshade_focused.svg
-#ln -s unshade.svg unshade_focused_normal.svg
-#ln -s unshade_focused_prelight.svg unshade_unfocused_prelight.svg
-#ln -s unshade_focused_pressed.svg unshade_unfocused_pressed.svg
-#ln -s menu.svg menu_focused.svg
-#ln -s menu.svg menu_focused_normal.svg
-#ln -s menu_focused_prelight.svg menu_unfocused_prelight.svg
-#ln -s menu_focused_pressed.svg menu_unfocused_pressed.svg
+ln -s close.png close_focused_normal.png
+ln -s close_focused_prelight.png close_unfocused_prelight.png
+ln -s close_focused_pressed.png close_unfocused_pressed.png
+ln -s maximize.png maximize_focused_normal.png
+ln -s maximize_focused_prelight.png maximize_unfocused_prelight.png
+ln -s maximize_focused_pressed.png maximize_unfocused_pressed.png
+ln -s minimize.png minimize_focused_normal.png
+ln -s minimize_focused_prelight.png minimize_unfocused_prelight.png
+ln -s minimize_focused_pressed.png minimize_unfocused_pressed.png
+ln -s unmaximize.png unmaximize_focused_normal.png
+ln -s unmaximize_focused_prelight.png unmaximize_unfocused_prelight.png
+ln -s unmaximize_focused_pressed.png unmaximize_unfocused_pressed.png
 
 wait
 exit 0
