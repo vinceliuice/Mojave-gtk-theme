9e227a8e340b64252778e40ef1ca14ae65694849
vinceliuice <vinceliuice@hotmail.com>
2024-11-15 21:26:31 +0800

    Fixed gnome-shell 47 issues

--- install.sh
+++ install.sh
@@ -753,7 +753,9 @@ fi
 if [[ "$(command -v gnome-shell)" ]]; then
   echo; prompt -w "Desktop version: '$(gnome-shell --version)'";
   SHELL_VERSION="$(gnome-shell --version | cut -d ' ' -f 3 | cut -d . -f -1)"
-  if [[ "${SHELL_VERSION:-}" -ge "46" ]]; then
+  if [[ "${SHELL_VERSION:-}" -ge "47" ]]; then
+    GS_VERSION="47-0"
+  elif [[ "${SHELL_VERSION:-}" -ge "46" ]]; then
     GS_VERSION="46-0"
   elif [[ "${SHELL_VERSION:-}" -ge "44" ]]; then
     GS_VERSION="44-0"
@@ -764,9 +766,9 @@ if [[ "$(command -v gnome-shell)" ]]; then
   else
     GS_VERSION="3-28"
   fi
-  else
-    prompt -e "'gnome-shell' not found, using styles for last gnome-shell version available."
-    GS_VERSION="46-0"
+else
+  prompt -e "'gnome-shell' not found, using styles for last gnome-shell version available."
+  GS_VERSION="47-0"
 fi
 
 if [[ "${gdm:-}" != 'true' && "${revert:-}" != 'true' ]]; then
